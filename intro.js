b84e1dbb361041daee59d9fba31d44a02d9a4a0d
williamoyewole5-sudo <williamoyewole5@gmail.com>
2026-02-14 15:52:45 +0100

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,8 @@
+// intro.js
+const name = "Oyewole Williams";
+const age = 19;
+const favoriteLanguage = "html";
+
+console.log(`Name: ${name}`);
+console.log(`Age: ${age}`);
+console.log(`Favorite Programming Language: ${favoriteLanguage}`);
